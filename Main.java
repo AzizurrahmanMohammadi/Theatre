bda3a40d0331281e965a4a5a73b610bd9aa72010
Aziz1212 <Azizurrahmanmohammadi12@gmail.com>
2017-06-09 14:53:41 +0430

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package Collection;
+
+public class Main {
+	  public static void main(String[] args) {
+		    Theatre theatre = new Theatre("Olympian", 8, 12);
+		    
+	        if(theatre.reserveSeat("H11")) {
+	            System.out.println("Please pay");
+	        }
+	        else {
+	            System.out.println("Sorry, seat is taken");
+	        }
+	        if(theatre.reserveSeat("H11")) {
+	            System.out.println("Please pay");
+	        } 
+	        else {
+	            System.out.println("Sorry, seat is taken");
+	        }
+
+	  }}
